c4df11a4fb914fe3b7b407aa745dcded5636edfe
Kshitij Gupta <kshitijgm@gmail.com>
2018-10-20 15:49:42 +0530

    Introduce overlay for Lawnchair default conf
    Change-Id: Ie05eaea58ed341d0f4f2f5dcc177663afe401de1
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- config/packages.mk
+++ config/packages.mk
@@ -12,6 +12,10 @@ PRODUCT_PACKAGES += \
     WellbeingPrebuilt \
     Updater
 
+# Lawnchair Default Configuration
+PRODUCT_PACKAGES += \
+    LawnConf
+
 # Filesystems tools
 PRODUCT_PACKAGES += \
     fsck.exfat \
